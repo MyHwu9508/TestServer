7140e2cfb6fb848893838ab77fb0f10268a8f9e4
Ronny <ronnyhuawei13@gmail.com>
2018-09-14 00:08:10 +0200

    car und Einhornmaske

--- ragemp-srv/packages/TEST/commands.js
+++ ragemp-srv/packages/TEST/commands.js
@@ -9,3 +9,11 @@ mp.events.addCommand('hp', (player) => {
 mp.events.addCommand('armor', (player) => {
     player.armour = 100;
 });
+
+mp.events.addCommand('veh', (player) => {
+    mp.vehicles.new(mp.joaat("turismor"), new mp.Vector3(-441.88, 1156.86, 326),
+    {
+        numberPlate: "ADMIN",
+        color: [[255,105,180],[255,105,180]]
+    });
+});
--- ragemp-srv/packages/TEST/events.js
+++ ragemp-srv/packages/TEST/events.js
@@ -4,3 +4,7 @@ mp.events.add('playerDeath', (player) => {
     player.spawn(spawnPoints[Math.floor(Math.random() * spawnPoints.length)]);
     player.health = 100;
 });
+
+mp.events.add('playerJoin', (player) => {
+    player.setClothes(1, 98, 0, 1)
+});
