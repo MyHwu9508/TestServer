35b09b7e276f60d96d748f8ad2b8627a17d298f1
Ronny <ronnyhuawei13@gmail.com>
2018-09-13 23:54:33 +0200

    reset

--- ragemp-srv/packages/commands.js
+++ ragemp-srv/packages/commands.js
@@ -9,11 +9,3 @@ mp.events.addCommand('hp', (player) => {
 mp.events.addCommand('armor', (player) => {
     player.armour = 100;
 });
-
-mp.events.addCommand('veh', (player) => {
-    mp.vehicles.new(mp.joaat("turismor"), new mp.Vector3(-441.88, 1156.86, 326),
-    {
-        numberPlate: "Kaniggel",
-        color: [[255,105,180],[255,105,180]]
-    });
-});
--- ragemp-srv/packages/events.js
+++ ragemp-srv/packages/events.js
@@ -4,7 +4,3 @@ mp.events.add('playerDeath', (player) => {
     player.spawn(spawnPoints[Math.floor(Math.random() * spawnPoints.length)]);
     player.health = 100;
 });
-
-mp.events.add('playerJoin', (player) => {
-    player.setClothes(1, 98, 0, 1)
-});
