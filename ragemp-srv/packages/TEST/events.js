7c721a3604aecf9ad724607b04cff51a0b73d2c0
Ronny <ronnyhuawei13@gmail.com>
2018-09-14 12:43:47 +0200

    Clothing :3

--- ragemp-srv/packages/TEST/events.js
+++ ragemp-srv/packages/TEST/events.js
@@ -6,5 +6,11 @@ mp.events.add('playerDeath', (player) => {
 });
 
 mp.events.add('playerJoin', (player) => {
-    player.setClothes(1, 98, 0, 1)
+    player.setClothes(1, 98, 0, 2) //mask
+    player.setClothes(4, 67, 6, 2) //Hose
+    player.setClothes(6, 47, 6, 2) //Schuhe
+    player.setClothes(11, 148, 6, 2) //Oberteil
+    player.setClothes(3, 110, 6, 2) //Handschuhe
+    player.setClothes(10, 0, 0, 2) //Sticker
+    player.setClothes(8, 0, -1, 2) //Hemd
 });
