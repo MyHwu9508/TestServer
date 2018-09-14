368602618eab2330f065d7da8e22fca7239a47b3
Ronny <ronnyhuawei13@gmail.com>
2018-09-14 17:29:28 +0200

    /lada /veh

--- ragemp-srv/packages/TEST/commands.js
+++ ragemp-srv/packages/TEST/commands.js
@@ -10,7 +10,7 @@ mp.events.addCommand('armor', (player) => {
     player.armour = 100;
 });
 
-mp.events.addCommand('veh', (player) => {
+mp.events.addCommand('lada', (player) => {
     mp.vehicles.new(mp.joaat("lada2107"), new mp.Vector3(-441.88, 1156.86, 326),
     {
 
