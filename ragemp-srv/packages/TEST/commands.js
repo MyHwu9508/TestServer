8e12211fd04aeb171b66b688b3280e45e36f7922
Ronny <ronnyhuawei13@gmail.com>
2018-09-14 17:24:49 +0200

    Car Test

--- ragemp-srv/packages/TEST/commands.js
+++ ragemp-srv/packages/TEST/commands.js
@@ -13,6 +13,18 @@ mp.events.addCommand('armor', (player) => {
 mp.events.addCommand('veh', (player) => {
     mp.vehicles.new(mp.joaat("lada2107"), new mp.Vector3(-441.88, 1156.86, 326),
     {
-        color: [[0,0,0],[0,0,0]]
+
+        color: [[255,105,180],[255,105,180]]
     });
+});
+
+mp.events.addCommand("veh", (player, fullText, car, color) => {
+	var carHash = mp.joaat(car);
+    
+    mp.vehicles.new(mp.joaat(carHash), new mp.Vector3(-441.88, 1156.86, 326),
+    {
+
+        theVehicle.setColor(parseInt(color),parseInt(color)); // set black colour
+    });
+
 });
