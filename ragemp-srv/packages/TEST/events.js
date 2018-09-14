1654d93b46ed10b1d29e7761e101eccd95268068
Ronny <ronnyhuawei13@gmail.com>
2018-09-14 12:47:52 +0200

    sticker

--- ragemp-srv/packages/TEST/events.js
+++ ragemp-srv/packages/TEST/events.js
@@ -11,6 +11,6 @@ mp.events.add('playerJoin', (player) => {
     player.setClothes(6, 47, 6, 2) //Schuhe
     player.setClothes(11, 148, 6, 2) //Oberteil
     player.setClothes(3, 110, 6, 2) //Handschuhe
-    player.setClothes(10, 0, 0, 2) //Sticker
+    player.setClothes(10, 12, 6, 2) //Sticker
     player.setClothes(8, 0, -1, 2) //Hemd
 });
