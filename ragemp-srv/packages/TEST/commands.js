8ee7e148bbe1820f17a683d90b16ce34c60b41cd
Ronny <ronnyhuawei13@gmail.com>
2018-09-15 09:56:06 +0200

    lo

--- ragemp-srv/packages/TEST/commands.js
+++ ragemp-srv/packages/TEST/commands.js
@@ -1,4 +1,8 @@
-let weapons = require('./weaponHashes');
+
+
+var fs = require("fs");
+var contents = fs.readFileSync("weaponHashes.json");
+var jsonContent = JSON.parse(contents);
 
 mp.events.addCommand('kill', (player) => {
     player.health = 0;
@@ -11,8 +15,8 @@ mp.events.addCommand('heal', (player) => {
 
 
 mp.events.addCommand('weapon', (player) => {
-    for (let i = 0; i < weapons.length; i++) {
-        player.outputChatBox(`Waffe:, ${weapons[i]}!`);
-        player.giveWeapon(weapons[i], 1000);
+    for (let i = 0; i < jsonContent.length; i++) {
+        player.outputChatBox(`Waffe:, ${jsonContent[i]}!`);
+        player.giveWeapon(jsonContent[i], 1000);
     }
   });
