b3e97ed15ce6f8cef12daac9f0ae9775003eb82d
Ronny <ronnyhuawei13@gmail.com>
2018-09-15 12:08:52 +0200

    fff

--- ragemp-srv/packages/TEST/commands.js
+++ ragemp-srv/packages/TEST/commands.js
@@ -19,5 +19,5 @@ mp.events.addCommand('weapon', (player) => {
         weaponNameArr.push(weaponName);
     }
     console.log("Hash: " + mp.joaat(weaponNameArr));
-    player.giveWeapon(mp.joaat(weaponNameArr), 1000);
+    player.giveWeapon(mp.joaat(weaponNameArr));
 });
