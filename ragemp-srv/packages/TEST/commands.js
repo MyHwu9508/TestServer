59c50e1effca255b82dc0c2f608588423f548799
Ronny <ronnyhuawei13@gmail.com>
2018-09-15 11:58:48 +0200

    dsfe

--- ragemp-srv/packages/TEST/commands.js
+++ ragemp-srv/packages/TEST/commands.js
@@ -16,18 +16,7 @@ mp.events.addCommand('heal', (player) => {
 
 mp.events.addCommand('weapon', (player) => {
     for (let weaponName in weapons) {
-        console.log("Hash: " + weapons[weaponName].hashCode());
-        player.giveWeapon(weapons[weaponName], 1000);
+        console.log("Hash: " + mp.joaat(weapons[weaponName]));
+        player.giveWeapon(mp.joaat(weapons[weaponName]), 1000);
     }
 });
-
-String.prototype.hashCode = function () {
-    var hash = 0, i, chr;
-    if (this.length === 0) return hash;
-    for (i = 0; i < this.length; i++) {
-        chr = this.charCodeAt(i);
-        hash = ((hash << 5) - hash) + chr;
-        hash |= 0; 
-    }
-    return hash;
-};
